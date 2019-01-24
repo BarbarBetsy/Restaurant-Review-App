b237ff244b405a3af2b85d5a6544e5226cf7e74c
BarbarBetsy <barbarbetsy@googlemail.com>
2019-01-24 21:29:31 +0100

    feat: Add map

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYmFyYmFyYmV0c3kiLCJhIjoiY2pyYjAxdWVpMDZibDQzbXIyN3ZudnZhcyJ9.mT-UfPq8mf9Vk9rDQK2Ldg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
